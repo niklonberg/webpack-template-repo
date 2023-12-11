747b4ba2fe260a59082f059dba61966fe4575c8c
Nikolai Lønberg <niklonberg@gmail.com>
2023-12-11 18:29:57 +0100

    initial setup of webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./index.css";
+const log = console.log;
+log("hello world");
+
+function component() {
+  const p = document.createElement("p");
+  p.textContent = "Hi there";
+
+  p.classList.add("hello");
+
+  return p;
+}
+
+document.body.appendChild(component());
