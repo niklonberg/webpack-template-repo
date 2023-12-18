78eaf1f1e41a8a6f331d7362d1931013e73480b5
Nikolai Lønberg <niklonberg@gmail.com>
2023-12-18 11:17:01 +0100

    add html-webpack-plugin and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,6 +8,15 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Change Me In Webpack.config",
+      template: "./src/index-template.html",
+      filename: "index.html", // specify name om html file
+      inject: "body", // injects js/ts files into end of body,
+      // scriptLoading: "defer", | if you set inject to head instead, this will improve page startup perf.
+    }),
+  ],
   devtool: "inline-source-map",
   module: {
     rules: [
